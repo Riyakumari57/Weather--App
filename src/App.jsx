eef470c736df75eafb242e5d548558fc66b2fbe8
Riya <riyak3448@gmail.com>
2023-07-16 21:38:28 +0530

    Files

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+import Temp2 from './Components/Temp2'
+import Temp3 from './Components/Temp3'
+import TempApp from './Components/TempApp'
+
+function App() {
+
+  return (
+    <>
+      {/* <TempApp></TempApp> */}
+      {/* <Temp2></Temp2> */}
+      <Temp3></Temp3>
+    </>
+  )
+}
+
+export default App
---
+++ src/Components/Temp2.jsx
@@ -0,0 +1,81 @@
+import React, { useEffect, useState , useRef} from 'react'
+
+function TempApp() {
+    const [city , setCity] = useState(null);
+    const [search , setSearch] = useState(null);
+    const inputRef = useRef(null);
+     
+  
+
+    useEffect(()=>{
+          
+        const fetchApi = async()=>{
+            const url = `https://api.openweathermap.org/data/2.5/weather?q=${search}&units=metric&appid=b28f76e2bb19a708450e0ca7e524e24a`
+            const response = await fetch(url);
+            const result = await response.json();
+            console.log(result);
+            setCity(result.main);
+        }
+
+        const clearData = ()=>{
+          inputRef.current.value = "";
+        }
+           fetchApi();
+           clearData();
+    },[search])
+  
+  
+    const handleClick = () => {
+      setSearch(inputRef.current.value);
+     
+    };
+
+  
+  
+  
+    return (
+    <div>
+         <div className="box">
+            <div className="inputData">
+              <input type="text" ref={inputRef}
+                className="inputField"
+                // value={search}
+                />
+                <i className="searchButton fa-solid fa-magnifying-glass" onClick={handleClick}></i>
+            </div>
+
+           {!city ? (
+            <p className='ErrorMessage'>No Data Found</p>
+           )
+        :( 
+            <div>
+            
+            <div className="info">
+            <h1 className="location">
+               <i className="fa-sharp fa-solid fa-location-dot"></i> {search}
+            </h1>
+
+            <h1 className="temp">
+                {city.temp}° Cel
+            </h1>
+
+            <h3 className="tempmin_max">
+              Min:{city.temp_min}° Cel | Max:{city.temp_max}° Cel
+            </h3>
+       </div>
+       </div>
+
+        )}
+
+         <div className="wave -one"></div>
+         <div className="wave -two"></div>
+         <div className="wave -three"></div>
+         
+         </div>
+
+         
+    </div>
+  )
+}
+
+export default TempApp
