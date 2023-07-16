61bd92910de68c1643fb1680bc802d64fad04080
Riya <riyak3448@gmail.com>
2023-07-16 22:31:14 +0530

    files

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,14 @@
 import './App.css'
 import Temp2 from './Components/Temp2'
-import Temp3 from './Components/Temp3'
-import TempApp from './Components/TempApp'
+// import TempApp from './Components/TempApp'
 
 function App() {
 
   return (
     <>
       {/* <TempApp></TempApp> */}
-      {/* <Temp2></Temp2> */}
-      <Temp3></Temp3>
+      <Temp2></Temp2>
+      {/* <Temp3></Temp3> */}
     </>
   )
 }
